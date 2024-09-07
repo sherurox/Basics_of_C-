f119e4440ca187a399e6bc2b42051f2f4105b9d1
Shreyas <78716247+sherurox@users.noreply.github.com>
2024-09-06 23:32:17 -0400

    Add files via upload
    Signed-off-by: Shreyas <78716247+sherurox@users.noreply.github.com>

---
+++ Templates/Function Templates with Parameters/funcc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+/*float funcAverage(int a, int b){
+    float avg= (a+b)/2.0; 
+    return avg;
+}*/
+
+template<class T1, class T2>
+float funcAverage(T1 a, T2 b){
+    float avg= (a+b)/2.0; 
+    return avg;
+}
+
+int main(){
+    float a;
+    a = funcAverage(5,2.8);
+    printf("The average of these numbers is %f",a);
+    return 0;
+}
